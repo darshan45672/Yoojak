0bdc99fb2ef51c2b741bd309989bbe943000e556
darshan45672 <drshnbhandary@gmail.com>
2024-02-24 18:43:13 +0530

    updated layout.php

--- resources/views/app/layout.blade.php
+++ resources/views/app/layout.blade.php
@@ -21,7 +21,6 @@
     <link href="{{ asset('css/jquery.mb.YTPlayer.min.css') }}" media="all" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     {{-- <link rel="stylesheet" type="text/css" href="{{ asset('user/css/style.css') }}" /> --}}
-
 </head>
 <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
     <div class="site-wrap">
