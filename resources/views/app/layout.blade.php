0304510a2e92995fc4a1e6c958ff4cdac2647b2d
darshan45672 <drshnbhandary@gmail.com>
2024-02-24 18:43:24 +0530

    updated layout.php

--- resources/views/app/layout.blade.php
+++ resources/views/app/layout.blade.php
@@ -6,7 +6,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-
     <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700,900" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('fonts/icomoon/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
